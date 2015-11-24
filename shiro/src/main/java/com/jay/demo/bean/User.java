12011f6a9f14264d9ac6cbf8dabb414c88a6311f
magang <mg6521018@126.com>
2015-11-24 20:01:51 +0800

    this is first version

---
+++ shiro/src/main/java/com/jay/demo/bean/User.java
@@ -0,0 +1,52 @@
+package com.jay.demo.bean;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+public class User implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private String id;
+	private String username;
+	private String password;
+	private Set<Role> roleSet = new HashSet<Role>();
+	
+	public User() {
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Set<Role> getRoleSet() {
+		return roleSet;
+	}
+
+	public void setRoleSet(Set<Role> roleSet) {
+		this.roleSet = roleSet;
+	}
+
+	
+}
